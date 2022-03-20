68b238f89073289d77dc4383c756fc6f724ac9f7
Yussif <djiceman.gh@gmail.com>
2022-03-20 22:22:06 +0000

    update

---
+++ app/Http/Controllers/API/Contactcontroller.php
@@ -0,0 +1,71 @@
+<?php
+
+namespace App\Http\Controllers\API;
+
+use App\Http\Controllers\Controller;
+use App\Models\Contacts;
+use Illuminate\Http\Request;
+
+class Contactcontroller extends Controller
+{
+    /**
+     * Display a listing of the resource.
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function index()
+    {
+        $contacts = Contacts::all();
+        return response()->json($contacts, 200);
+    }
+
+    /**
+     * Store a newly created resource in storage.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @return \Illuminate\Http\Response
+     */
+    public function store(Request $request)
+    {
+        $contact = Contacts::create($request->all());
+        return response()->json($contact, 200);
+    }
+
+    /**
+     * Display the specified resource.
+     *
+     * @param  int  $id
+     * @return \Illuminate\Http\Response
+     */
+    public function show($id)
+    {
+        $contact = Contacts::findOrFail($id);
+        return response()->json($contact, 200);
+    }
+
+    /**
+     * Update the specified resource in storage.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @param  int  $id
+     * @return \Illuminate\Http\Response
+     */
+    public function update(Request $request, $id)
+    {
+        $contact = Contacts::findOrFail($id);
+        $contact->update($request->all());
+        return response()->json($contact, 200);
+    }
+
+    /**
+     * Remove the specified resource from storage.
+     *
+     * @param  int  $id
+     * @return \Illuminate\Http\Response
+     */
+    public function destroy($id)
+    {
+        $contact = Contacts::findOrFail($id);
+        $contact->delete();
+    }
+}
--- routes/api.php
+++ routes/api.php
@@ -1,6 +1,7 @@
 <?php
 
 use App\Http\Controllers\API\Todocontroller;
+use App\Http\Controllers\API\Contactcontroller;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
 
@@ -20,3 +21,4 @@ Route::middleware('auth:sanctum')->get('/user', function (Request $request) {
 });
 
 Route::apiResource('todos', Todocontroller::class);
+Route::apiResource('contacts', Contactcontroller::class);
